2ae0df70240acafdbf24891de6615932a5f7d2c2
Roman Krejcik <farin@farin.cz>
2017-07-09 11:39:52 +0200

    stateless placement history

--- src/main/java/com/jcloisterzone/ui/grid/layer/PlacementHistory.java
+++ src/main/java/com/jcloisterzone/ui/grid/layer/PlacementHistory.java
@@ -5,13 +5,11 @@ import java.awt.Color;
 import java.awt.Composite;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
 
-import com.google.common.eventbus.Subscribe;
 import com.jcloisterzone.IPlayer;
 import com.jcloisterzone.board.Position;
+import com.jcloisterzone.event.PlayEvent;
+import com.jcloisterzone.event.PlayerTurnEvent;
 import com.jcloisterzone.event.TileEvent;
 import com.jcloisterzone.ui.GameController;
 import com.jcloisterzone.ui.ImmutablePoint;
@@ -26,118 +24,63 @@ public class PlacementHistory extends AbstractGridLayer {
     private static final ImmutablePoint SHADOW_POINT = new ImmutablePoint(51,51);
     private static final AlphaComposite ALPHA_COMPOSITE = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .6f);
 
-    private final LinkedList<PlacementHistoryEntry> entries = new LinkedList<PlacementHistoryEntry>();
-    private List<PlacementHistoryEntry> visibleEntries;
 
     public PlacementHistory(GridPanel gridPanel, GameController gc) {
         super(gridPanel, gc);
-
         gc.register(this);
-    }
-
-    @Subscribe
-    public void handleTileEvent(TileEvent ev) {
-        if (TileEvent.PLACEMENT == ev.getType()) {
-            entries.addFirst(createPlacementHistoryEntry(ev));
-        } else if (TileEvent.REMOVE == ev.getType()) {
-            entries.remove(createPlacementHistoryEntry(ev));
-        }
-
-        int limit = 0;
-        int ndx = 0;
-        IPlayer lastPlayer = null;
-        IPlayer previousPlayer = null;
-        for (PlacementHistoryEntry entry : entries) {
-
-            if (ndx == 0) {
-                lastPlayer = previousPlayer = entry.player;
-            }
-
-            if (lastPlayer == null) {
-                // player can be null (load, river-end)
-                break;
-            }
-
-            if (previousPlayer != entry.player && lastPlayer == entry.player) {
-                // complete round
-                limit = ndx;
-                break;
-            }
-
-            previousPlayer = entry.player;
-            ndx++;
-        }
-
-        if (limit == 0) {
-            limit = getGame().getAllPlayers().length();
-        }
-
-        visibleEntries = entries.subList(0, Math.min(entries.size(), limit));
     }
 
     @Override
     public void paint(Graphics2D g) {
-
-        if (visibleEntries == null) {
-            return;
-        }
-
         Composite oldComposite = g.getComposite();
         g.setComposite(ALPHA_COMPOSITE);
 
+        IPlayer turnPlayer = getGame().getTurnPlayer();
         int counter = 0;
-        for (PlacementHistoryEntry entry : visibleEntries) {
+
+        boolean breakOnTurnEvent = false;
+        boolean turnEventSeen = false;
+        Boolean placedCurrentTurn = null;
+
+        for (PlayEvent ev : gc.getGame().getState().getEvents().reverseIterator()) {
+            if (ev instanceof PlayerTurnEvent) {
+                if (breakOnTurnEvent) break;
+
+                turnEventSeen = true;
+                if (placedCurrentTurn == null) placedCurrentTurn = false;
+
+                IPlayer p = ev.getTargetPlayer();
+                if (p != null && getGame().getPrevPlayer(p).equals(turnPlayer)) {
+                    if (placedCurrentTurn) {
+                        break;
+                    } else {
+                        breakOnTurnEvent = true;
+                    }
+                }
+            }
+
+            if (!(ev instanceof TileEvent)) continue;
+            TileEvent te = (TileEvent) ev;
+            if (te.getType() != TileEvent.PLACEMENT) continue;
+
+            if (placedCurrentTurn == null && !turnEventSeen) {
+                placedCurrentTurn = true;
+            }
+
+            Position pos = te.getPosition();
+            IPlayer player = te.getTriggeringPlayer();
             String text = String.valueOf(++counter);
-            Color color = entry.player != null ?  entry.player.getColors().getFontColor() : DEFAULT_COLOR;
+            Color color = player != null ?  player.getColors().getFontColor() : DEFAULT_COLOR;
 
             BufferedImage buf = UiUtils.newTransparentImage(getTileWidth(), getTileHeight());
             Graphics2D gb = (Graphics2D) buf.getGraphics();
             drawAntialiasedTextCentered(gb, text, 80, ZERO, POINT, color, null);
             gb.setComposite(AlphaComposite.DstOver);
             drawAntialiasedTextCentered(gb, text, 80, ZERO, SHADOW_POINT, Color.GRAY, null);
-            g.drawImage(buf, null, getOffsetX(entry.position), getOffsetY(entry.position));
+            g.drawImage(buf, null, getOffsetX(pos), getOffsetY(pos));
+
         }
 
         g.setComposite(oldComposite);
-    }
-
-    private PlacementHistoryEntry createPlacementHistoryEntry(TileEvent tileEvent) {
-        return new PlacementHistoryEntry(tileEvent.getTriggeringPlayer(), tileEvent.getPosition());
-    }
-
-    private class PlacementHistoryEntry {
-        private final IPlayer player;
-        private final Position position;
-
-        public PlacementHistoryEntry(IPlayer player, Position position) {
-            this.player = player;
-            this.position = position;
-        }
-
-        private PlacementHistory getOuterType() {
-            return PlacementHistory.this;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(getOuterType(), player, position);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-
-            PlacementHistoryEntry that = (PlacementHistoryEntry) obj;
-
-            return Objects.equals(this.getOuterType(), that.getOuterType())
-                    && Objects.equals(this.player, that.player)
-                    && Objects.equals(this.position, that.position);
-        }
-
     }
 }
